bbaf0a38f0037215134457b4e9c134fc3efa621c
lcchua <laichwang@gmail.com>
2024-11-11 16:02:26 +0800

    fix more checkov reports

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -7,6 +7,7 @@
 
 # S3 bucket creation
       #checkov:skipped=CKV2_AWS_62:Ensure S3 buckets should have event notifications enabled
+      #checkov:skipped=CKV_AWS_144:Ensure that S3 bucket has cross-region replication enabled            
       #  Custom IAM policy attached to this S3 resource
 resource "aws_s3_bucket" "this" {
   #  bucket = "${var.ml_s3bucket_name}-${random_id.suffix_s3.dec}"
@@ -21,6 +22,17 @@ resource "aws_s3_bucket" "this" {
 output "s3bucket" {
   description = "dev stw S3 bucket"
   value       = aws_s3_bucket.this.id
+}
+
+# Fix CKV_AWS_145
+resource "aws_s3_bucket_server_side_encryption_configuration" "good_sse_1" {
+  bucket = aws_s3_bucket.this.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
 
 # Enable bucket versioning
@@ -48,13 +60,13 @@ resource "aws_s3_bucket_ownership_controls" "this-owner-ctl" {
 }
 
 # Disable block all public accesses
-resource "aws_s3_bucket_public_access_block" "this-pub-access-blk" {
-  bucket = aws_s3_bucket.this.id
       #checkov:skipped=CKV_AWS_56:Ensure S3 bucket has 'restrict_public_buckets' enabled
       #checkov:skipped=CKV_AWS_55:Ensure S3 bucket has ignore public ACLs enabled
       #checkov:skipped=CKV_AWS_54:Ensure S3 bucket has block public policy enabled
       #checkov:skipped=CKV_AWS_53:Ensure S3 bucket has block public ACLS enabled
       #  Custom IAM policy attached to this S3 resource
+resource "aws_s3_bucket_public_access_block" "this-pub-access-blk" {
+  bucket = aws_s3_bucket.this.id
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
@@ -92,6 +104,22 @@ resource "aws_s3_object" "folder2" {
   source = "/dev/null"
 }
 
+# Configure the S3 bucjet logging
+#   https://kodekloud.com/blog/how-to-create-aws-s3-bucket-using-terraform/ 
+      #checkov:skipped=CKV_AWS_144:Ensure that S3 bucket has cross-region replication enabled
+resource "aws_s3_bucket" "logging_bucket" {
+  bucket = "${aws_s3_bucket.this.id}-logging-bucket"
+}
+output "logging_bucket" {
+  description = "dev stw loggin S3 bucket"
+  value       = aws_s3_bucket.logging_bucket.id
+}
+
+resource "aws_s3_buck_acl" "log_bucket_acl" {
+  bucket= aws_s3_bucket.logging_bucket.id
+  acl = "log-delivery-write"
+}
+
 # Enable bucket access logging and creation of a specific "logging bucket"
 resource "aws_s3_bucket_logging" "this_logging" {
   bucket = aws_s3_bucket.this.id
@@ -101,12 +129,15 @@ resource "aws_s3_bucket_logging" "this_logging" {
   target_prefix = "log/"
 }
 
-resource "aws_s3_bucket" "logging_bucket" {
-  bucket = "${aws_s3_bucket.this.id}-logging-bucket"
-}
-output "logging_bucket" {
-  description = "dev stw loggin S3 bucket"
-  value       = aws_s3_bucket.logging_bucket.id
+# Fix CKV_AWS_145
+resource "aws_s3_bucket_server_side_encryption_configuration" "good_sse_2" {
+  bucket = aws_s3_bucket_logging.this_logging.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
 
 # Define the custom bucket IAM policy for DVC accesses
