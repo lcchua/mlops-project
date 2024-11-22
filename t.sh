f34601916e38a46788af6a2eb72883603583e857
lcchua <laichwang@gmail.com>
2024-11-22 14:16:48 +0800

    testing branch triggers

--- t.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-aws ecr describe-images --repository ce7-grp-1/nonprod/predict_buy_app --region us-east-1 \
-	--query "sort_by(imageDetails, &imagePushedAt)[-1].imageTags" --output text | \
-	grep -oE 'v[0-9]+\.[0-9]+\.[0-9]+-[A-Z]{3}\.[0-9]+'
